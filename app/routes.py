23fc7e60bcb8baedb755038619ef160b689d3c34
explosion33 <38818379+explosion33@users.noreply.github.com>
2020-09-13 23:47:34 -0700

    added basig page template, started dynamic page.html

--- app/routes.py
+++ app/routes.py
@@ -10,10 +10,75 @@ log = logging.getLogger('werkzeug')
 log.setLevel(logging.ERROR)
 
 
+def getNavbar(route):
+    navbar=[
+        {
+            "link": "/academics",
+            "name": "Academics",
+            "active": "",
+        },
+        {
+            "link": "/programming",
+            "name": "Programming",
+            "active": "",
+        },
+        {
+            "link": "/projects",
+            "name": "Projects",
+            "active": "",
+        },
+        {
+            "link": "/volunteering",
+            "name": "Volunteering",
+            "active": "",
+        },
+        {
+            "link": "/rocketry",
+            "name": "Rocketry",
+            "active": "",
+        },
+        {
+            "link": "/sports",
+            "name": "Sports",
+            "active": "",
+        }, 
+    ]
+
+    for item in navbar:
+        if item["link"] == request.path:
+            item["active"] = "active"
+    
+    return navbar
+
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template("main.html", head="Start of Website")
 
+@app.route('/academics', methods=['GET', 'POST'])
+def academics():
+    return render_template("page.html", navbar=getNavbar(request.path))
+
+@app.route('/programming', methods=['GET', 'POST'])
+def programming():
+    return render_template("page.html", navbar=getNavbar(request.path))
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+    return render_template("page.html", navbar=getNavbar(request.path))
+
+@app.route('/volunteering', methods=['GET', 'POST'])
+def volunteering():
+    return render_template("page.html", navbar=getNavbar(request.path))
+
+@app.route('/rocketry', methods=['GET', 'POST'])
+def rocketry():
+    return render_template("page.html", navbar=getNavbar(request.path))
+
+@app.route('/sports', methods=['GET', 'POST'])
+def sports():
+    return render_template("page.html", navbar=getNavbar(request.path))
 
 @app.after_request
 def add_header(r):
